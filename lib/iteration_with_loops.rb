f6fe00b613e467a16c8902886ed772f2f4cc8e30
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-13 15:14:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,23 +1,27 @@
 def find_min_in_nested_arrays(src)
-  index = 0 
-  smallest_number_array = []
-  while index < src.length do
-    inner_index = 0 
-    inner_array = src[index]
-    while inner_index < inner_array.length do 
-      if inner_array[inner_index][0] < inner_array[inner_index]
-        smallest_number_array.push(inner_array[inner_index])
-      end 
-      smallest_number_array.push(smallest_number)
-      inner_index += 1
-    end
-    index += 1
-    
-  end 
-  
-  
-  #checking each nested for its smallest number and returning it in a new array
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
-smallest_number_array
+  #creat an index variable and empty Array
+  #itterate thgriough first Array
+  #set inner array index to 0 
+ #itterate through second array
+ #set smallest number variable equal to the inner array at 0 
+ # check to see if the current value of the current iteration is less that smallest number andd reset #outside of the if condiional, and inner itteration push on the smallest value to the empty array
+ #return empty array 
+ 
+ index = 0 
+ smallest_array = []
+ while index < src.length do 
+   inner_index = 0 
+   inner_array = src[index]
+   while inner_index < inner_array.length 
+      smallest_number = inner_array[0]
+      if inner_array[inner_index] < smallest_number
+        smallest_number = inner_array[inner_index]
+      end
+   inner_index += 1 
+   end
+   smallest_array.push(smallest_number)
+   index += 1 
+ end
+ 
+ smallest_array
 end
